f39e67eda7004c89e73c29c42c81425d9bff12d8
GitAniket98 <aniketchaudhary9838@gmail.com>
2025-12-24 11:24:17 +0000

    first-Commit : Added complete Fund-Me project

---
+++ script/Interaction.s.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function fundFundME(address mostRecentDeployedFundMe) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentDeployedFundMe)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("Funded FundMe with %s ETH", SEND_VALUE);
+    }
+
+    function run() external {
+        vm.startBroadcast();
+        address mostRecentDeployedFundMe = DevOpsTools
+            .get_most_recent_deployment("FundMe", block.chainid);
+        fundFundME(mostRecentDeployedFundMe);
+        vm.stopBroadcast();
+    }
+}
+
+contract WithdrawFundMe is Script {
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("Withdraw FundMe balance!");
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        withdrawFundMe(mostRecentlyDeployed);
+    }
+}
---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/Interaction.s.sol";
+
+contract InteractionsTest is Test {
+    uint256 number;
+    FundMe fundMe;
+    uint256 constant SEND_VALUE = 0.1 ether;
+    uint256 constant STARTING_BALANCE = 10 ether;
+    address USER = address(1);
+
+    function setUp() external {
+        DeployFundMe deployer = new DeployFundMe();
+        fundMe = deployer.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundAndOwnerWithdraw() public {
+        uint256 preUserBalance = address(USER).balance;
+        uint256 preOwnerBalance = address(fundMe.getOwner()).balance;
+        uint256 originalFundMeBalance = address(fundMe).balance;
+
+        // Using vm.prank to simulate funding from the USER address
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+
+        uint256 afterUserBalance = address(USER).balance;
+        uint256 afterOwnerBalance = address(fundMe.getOwner()).balance;
+
+        assert(address(fundMe).balance == 0);
+        assertEq(afterUserBalance + SEND_VALUE, preUserBalance);
+        assertEq(
+            preOwnerBalance + SEND_VALUE + originalFundMeBalance,
+            afterOwnerBalance
+        );
+    }
+}
